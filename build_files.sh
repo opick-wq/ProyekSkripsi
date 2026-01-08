d04c0cc1a7144a8c9214c2ee4c0d034dcc5ff37e
opick-wq <ikgsanikhsan93@gmail.com>
2026-01-09 06:39:14 +0700

    admin

--- build_files.sh
+++ build_files.sh
@@ -4,11 +4,17 @@ pip install -r requirements.txt
 # Buat folder staticfiles_build jika belum ada
 mkdir -p staticfiles_build
 
-# Jalankan collectstatic dan taruh hasilnya di staticfiles_build
-# --noinput artinya jangan tanya "Yes/No", langsung timpa saja
-# Gunakan python3 agar sesuai dengan pip yang digunakan
+# Jalankan collectstatic
 python3 manage.py collectstatic --noinput --clear
 
-# Jalankan migrasi database
+# Jalankan migrasi database (Membuat tabel)
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+# --- BAGIAN BARU: BUAT ADMIN OTOMATIS ---
+# Perintah ini akan mengecek: Jika user 'admin' belum ada, maka buatkan.
+# Username: admin
+# Email: admin@example.com
+# Password: admin123  (Bisa Anda ganti di sini)
+
+python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
