0444f3168331c67fbad13a7a8d4a32e75be7ea4e
opick-wq <ikgsanikhsan93@gmail.com>
2026-01-09 06:16:07 +0700

    ayo

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,3 @@ pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput --clear
-#hs
-git update-index --chmod=+x build_files.sh
-git commit -m "Make build script executable"
-git push
